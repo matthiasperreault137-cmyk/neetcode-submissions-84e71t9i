49f59a4300ea855cf2f1b1bc52445d39fa579aeb
Furet137 <matthiasperreault137@gmail.com>
2026-09-22 23:11:55 -0400

    Add: word-ladder - submission-11

---
+++ Data Structures & Algorithms/word-ladder/submission-11.py
@@ -0,0 +1,43 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if beginWord == endWord:
+            return 1
+        seen = set()
+        toVisit = deque()
+        toVisit.append((beginWord, 1))
+        seen.add(beginWord)
+        while toVisit:
+            popVal = toVisit.popleft()
+            word = popVal[0]
+            val = popVal[1]
+            print("pooped" + word)
+            for w in wordList:
+                repI = None
+                for i in range(len(word)):
+                    if word[i] != w[i]:
+                        print(word[i] + "char1" + w[i] + "char2")
+                        if repI != None:
+                            repI = None
+                            break
+                        repI = i
+                if repI == None:
+                    print(w + "is invalid")
+                    continue
+                temp = word[repI]
+                word = word[:repI] + w[repI] + word[repI + 1:]
+                if word == endWord:
+                    return val + 1
+                if word not in seen:
+                    toVisit.append((word, val + 1))
+                    seen.add(word)
+                    print(word)
+                    word = word[:repI] + temp + word[repI + 1:]
+                else:
+                    word = word[:repI] + temp + word[repI + 1:]
+                    continue
+        return 0
+
+            
+
+                         
+
